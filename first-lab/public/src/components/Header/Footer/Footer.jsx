ea9d2d6c4d527d791c73fa2c75859984fd9eb1d3
Ivan <tushchukivan@gmail.com>
2025-03-04 10:16:56 +0200

    task 12

---
+++ first-lab/public/src/components/Header/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 FlowerShop | Найкращі квіти для вас </p>
+    </footer>
+  );
+}
+
+export default Footer;
